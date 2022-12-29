be634ed50af08269530161703922228f0b91fe7f
Uhiene <princessuhiene@gmail.com>
2022-12-29 12:55:38 +0100

    i refactored the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Truffle and Tailwind</h4>
+      <h4 className="text-3xl text-[#01B0F1]">Hello World</h4>
     </div>
   )
 }
