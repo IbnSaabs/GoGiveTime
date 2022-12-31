4f602e0bb0e57d16d628f0f4a5c2850b53172325
Azubire Peter <azubirepeter@gmail.com>
2022-12-31 02:55:10 +0000

    added select interest page

---
+++ src/components/Interest/InterestPageLayout.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import svg1 from "../../assets/pattern-top.svg";
+import svg2 from "../../assets/pattern-bottom.svg";
+import { FiArrowLeft } from "react-icons/fi";
+import Header from "./Header";
+
+const InterestPageLayout = ({
+  className,
+  cardClassName,
+  title,
+  subTitle,
+  children,
+}) => {
+  return (
+    <>
+      <Header />
+      <div className={className}>
+        <img
+          src={svg1}
+          className="absolute top-0 left-0 w-1/2 sm:w-1/3"
+          alt=""
+        />
+        <img
+          src={svg2}
+          alt=""
+          className="absolute bottom-0 right-0 w-1/2 sm:w-1/3"
+        />
+        {/* card starts */}
+        <div className={cardClassName}>
+          <div
+            className={`overflow-y-auto ${
+              !title && " flex flex-col justify-center items-center"
+            }`}
+          >
+            {/* card header  */}
+            {title && (
+              <div className="mb-5 px-[22px] lg:px-16">
+                <div className="flex mb-3 justify-between items-center ">
+                  <div className=" text-xl hover:cursor-pointer">
+                    <FiArrowLeft />
+                  </div>
+                  <div className="text-xl sm:text-2xl  font-medium mx-auto">
+                    {title}
+                  </div>
+                </div>
+                <p className="text-center  font-normal text-xs sm:text-lg">
+                  {subTitle}
+                </p>
+              </div>
+            )}
+            {/* card body  */}
+            {children}
+          </div>
+        </div>
+        {/* card end  */}
+      </div>
+    </>
+  );
+};
+
+export default InterestPageLayout;
