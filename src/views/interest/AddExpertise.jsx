03e9473c936603a00c6142b1ba7809e49c8bfb61
Azubire Peter <azubirepeter@gmail.com>
2022-12-31 03:35:49 +0000

    added add expertise page

---
+++ src/views/interest/AddExpertise.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import radioButton from "../../assets/interest/radio_button.svg";
+import radioButttonClicked from "../../assets/interest/radio_button_clicked.svg";
+import Student from "../../components/Interest/Forms/Student";
+import Professional from "../../components/Interest/Forms/Professional";
+import CommunityLeader from "../../components/Interest/Forms/CommunityLeader";
+
+import InterestPageLayout from "../../components/Interest/InterestPageLayout";
+
+const AddExpertise = () => {
+  const [selected, setSelected] = React.useState(undefined);
+
+  const [select, setSelect] = React.useState([
+    {
+      id: "student",
+      name: "Student",
+    },
+    {
+      id: "professional",
+      name: "Professional",
+    },
+    {
+      id: "communityLeader",
+      name: "Community Leader",
+    },
+  ]);
+
+  return (
+    <>
+      <InterestPageLayout
+        className="relative h-[calc(100vh-73px)] flex justify-center"
+        cardClassName="flex flex-col shadow-lg rounded-3xl border max-h-[90%] h-fit  w-[90%] sm:w-3/6 bg-white z-10  py-5 mt-5"
+        title="Add Expertise"
+        subTitle="Your feed will be personalised based on your skills. Don’t worry
+                you can change it later."
+      >
+        {/* card body  */}
+        <div className="  px-2 lg:px-20 mb-5">
+          <p className="text-lg font-normal mb-2">Are you a</p>
+          <div className="flex gap-1 justify-between items-center ">
+            {select.map((item, index) => (
+              <button
+                key={item.id}
+                type="button"
+                className={`py-2 px-2 sm:px-5  text-[10px] sm:text-sm  focus:outline-none bg-[#F9FAFB]  rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-red-500 focus:z-10 focus:ring-1 focus:ring-red-500  flex items-center gap-1 sm:gap-3 ${
+                  item.id === selected && "text-red-500"
+                }`}
+                onClick={() => setSelected(item.id)}
+              >
+                <img
+                  src={item.id === selected ? radioButttonClicked : radioButton}
+                  alt="select expertise"
+                />
+                {item.name}
+              </button>
+            ))}
+          </div>
+        </div>
+        {/* form  */}
+
+        {selected === "student" && <Student />}
+        {selected === "professional" && <Professional />}
+        {selected === "communityLeader" && <CommunityLeader />}
+
+        {!selected && <div className="py-10"></div>}
+      </InterestPageLayout>
+    </>
+  );
+};
+
+export default AddExpertise;
