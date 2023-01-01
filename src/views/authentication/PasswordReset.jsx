2edfa1a62b7f95bf72201cbd67b2f98464533587
okafohenry <okafohenrie@gmail.com>
2023-01-01 11:33:19 +0100

    completed Password Reset Link page in viewsauth/passwordresetlinkresend.jsx, added comment and corrected route error in utils/routes/allRoutes.js

--- src/views/authentication/PasswordReset.jsx
+++ src/views/authentication/PasswordReset.jsx
@@ -3,9 +3,10 @@ import React from "react";
 
 import { AuthCard, AuthPageLayout } from "../../components/Auth";
 import Logo from "../../assets/auth/auth-logo.svg";
+import { Link } from "react-router-dom";
 
 export const PasswordReset = () => {
-    const handleResendEmail = () => {alert('working')}
+    const handleResendEmail = () => {alert('password  reset working')}
 
     return(
         <AuthPageLayout text='Sign in' path='/sign-in'>
@@ -18,7 +19,7 @@ export const PasswordReset = () => {
                 >                   
                     {/* Resend email link */}
                     <div className="text-center mt-5">
-                        <a onClick={handleResendEmail} className="text-[#01B0F1] text-md hover:cursor-pointer">Resend email link</a>
+                        <Link to='/forgot-password' className="text-[#01B0F1] text-md hover:cursor-pointer">Resend email link</Link>
                     </div>
                      {/* change email ID */}
                      <div className="text-center mt-3">
--- src/views/authentication/PasswordResetLinkResend.jsx
+++ src/views/authentication/PasswordResetLinkResend.jsx
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 import { AuthCard, AuthPageLayout } from "../../components/Auth";
 import Logo from "../../assets/auth/auth-logo.svg";
 
-export const PasswordResetLinkResend= () => {
+export const PasswordResetLinkResend = () => {
+    
+    const handleResendLinkEmail = () => {alert('resend link working')}
 
     return(
         <AuthPageLayout text='Sign in' path='/sign-in'>
@@ -14,34 +16,15 @@ export const PasswordResetLinkResend= () => {
                     alt='GGT Logo'
                     text='Forgot Password'
                     googleAuth={false} 
-                    subText={<>Provide your account’s email for which you want to rest your password</>}
-                >
-                    {/* Email input */}
-                    <div className="relative mt-7 mb-1">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-md text-gray-500 dark:text-gray-400">
-                            <MdOutlineLocalPostOffice />
-                        </div>
-                        <input 
-                            type="email" 
-                            id="email"
-                            onChange={handleInputChange}
-                            required
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                            placeholder="Your Email" 
-                            />
-                    </div>                    
-                    {isHelperText ? <p id="helper-text-explanation" class="mt-0 text-sm text-[#EC3138]">Please enter your registered email</p> : null}
-                    
-                    {/* Verify Button */}
-                    <SubmitButton
-                        className='mt-5 text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-100 font-medium rounded-lg text-sm  w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                        type="button"
-                        text='Reset Password'
-                        handleClick={handleForgotPassword}
-                      />                      
-                    {/* Cancel link */}
-                    <div className="text-center mt-3">
-                        <Link to='/sign-in' className="text-[#EC3138] text-md">Cancel</Link>
+                    subText={<>The email has been resent. You will receive an email with a link to reset your password.</>}
+                >                   
+                    {/* Resend email link */}
+                    <div className="text-center mt-5">
+                        <Link to='/forgot-password' className="text-[#01B0F1] text-md hover:cursor-pointer">Resend email link</Link>
+                    </div>
+                     {/* change email ID */}
+                     <div className="text-center mt-3">
+                        <a onClick={handleResendLinkEmail} className="text-[#01B0F1] text-md hover:cursor-pointer">Change email ID</a>
                     </div>
                 </AuthCard>
         </AuthPageLayout>
