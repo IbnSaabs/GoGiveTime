be1b958406f747761875a3e6d150386838aab557
Uhiene <princessuhiene@gmail.com>
2023-01-04 11:16:48 +0100

    i started working on the recommened page

--- src/components/RecommendedEvents.jsx
+++ src/components/RecommendedEvents.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const RecommendedEvents = () => {
   return (
     <div className='w-[308px] h-[891px] border border-blue-400'>
-
+        <div className='border-b [#F5F5F5] p-4 '>
+            <h1 className='text-[#6C6C6C] text-[16px] font-medium '>Recommended Events</h1>
+            
+        </div>
     </div>
   )
 }
